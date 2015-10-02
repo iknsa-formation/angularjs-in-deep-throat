932a0b8ee32fd4479afeb4a2128bdfedaabd12e9
Khalid Sookia <khalidsookia@gmail.com>
2015-10-03 01:32:40 +0200

    Added angular-route.js, templates of the app and configured the routes. Also added some links on the index page

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,32 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            controller: 'AppController',
+            templateUrl: 'templates/home.html'
+        })
+        .when('/about', {
+            controller: 'AboutController',
+            templateUrl: 'templates/about.html'
+        })
+        .when('/recrutement', {
+            controller: 'RecruitementController',
+            templateUrl: 'templates/recruitement.html'
+        })
+        .when('/quotation', {
+            controller: 'QuotationController',
+            templateUrl: 'templates/quotation.html'
+        })
+        .when('/contact', {
+            controller: 'ContactController',
+            templateUrl: 'templates/contact.html'
+        })
+        .when('/404', {
+            controller: 'NotFoundController',
+            templateUrl: 'templates/404.html'
+        })
+        .otherwise({redirectTo: '/404'})
+        ;
+});
+
