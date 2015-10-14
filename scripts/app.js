cfdd0d78fd5c8e0bdeb60cb84d566dc6b6d5a531
Khalid Sookia <khalidsookia@gmail.com>
2015-10-14 19:56:54 +0200

    Form basic control in views

--- scripts/app.js
+++ scripts/app.js
@@ -61,19 +61,8 @@ app.controller('NotFoundController', ['$scope', function($scope){
 app.controller('TestController', ['$scope', function($scope){
     console.log('TestController');
 
-    $scope.who = ' la famille';
-    $scope.what = ' bien ';
-}]);
-
-app.controller('ChildController', ['$scope', function($scope){
-    console.log('ChildController');
-
-    $scope.what = ' bien ou bien ';
-}]);
-
-app.controller('GrandChildController', ['$scope', function($scope){
-    console.log('GrandChildController');
-
-    $scope.who = ' cousine';
-    $scope.what = ' bien ou quoi ';
+    $scope.testSubmit = function(user) {
+        console.log(user);
+        console.log('ok now i control the datas before sending it to the backend where it will be controlled again');
+    }
 }]);
